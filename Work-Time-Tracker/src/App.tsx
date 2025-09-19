14ec3ec622cf00a9cde9b7963ba48d3d3e23ca97
AndrasCsorba <andreascs@mail.ch>
2025-09-19 11:05:30 +0200

    tailwind daisy ui

--- Work-Time-Tracker/src/App.tsx
+++ Work-Time-Tracker/src/App.tsx
@@ -2,34 +2,23 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
+export default function App() {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div
+      className="min-h-screen bg-base-200 flex items-center justify-center p-6"
+      data-theme="light">
+      <div className="card w-full max-w-sm bg-base-100 shadow-xl">
+        <div className="card-body space-y-3">
+          <h2 className="card-title">Tailwind + DaisyUI teszt</h2>
+          <input
+            type="text"
+            placeholder="Írj ide..."
+            className="input input-bordered w-full"
+          />
+          <button className="btn btn-primary">Mentés</button>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
